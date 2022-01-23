3ce4e7413a7a43fb7f25d722a8a7c41e9df07a14
Timo Bauer <timo.bauer97@gmail.com>
2022-01-23 14:25:46 +0100

    Added Structures for PowerMeter and PowerMeterState

---
+++ BoschSmartHomeSharp/mdl/PowerMeter.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoschSmartHome.mdl.PowerMeter
+{
+    public class PowerMeter
+    {
+        [JsonProperty("@type")]
+        public string Type { get; set; }
+        public string id { get; set; }
+        public string deviceId { get; set; }
+        public PowerMeterState.PowerMeterState state { get; set; }
+        public string path { get; set; }
+
+        public static string Serialize(PowerMeter state)
+        {
+            return JsonConvert.SerializeObject(state);
+        }
+
+        public static PowerMeter Deserialize(string json)
+        {
+            return JsonConvert.DeserializeObject<PowerMeter>(json);
+        }
+    }
+}
---
+++ BoschSmartHomeSharp/mdl/PowerMeterState.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoschSmartHome.mdl.PowerMeterState
+{
+    public class PowerMeterState
+    {
+        public int powerConsumption { get; set; }
+        public int energyConsumption { get; set; }
+
+        public static string Serialize(PowerMeterState state)
+        {
+            return JsonConvert.SerializeObject(state);
+        }
+
+        public static PowerMeterState Deserialize(string json)
+        {
+            return JsonConvert.DeserializeObject<PowerMeterState>(json);
+        }
+    }
+}
