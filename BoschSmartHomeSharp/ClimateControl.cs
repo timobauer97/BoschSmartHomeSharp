6d97cad24ea537ac30dd301ad3a203488c6fe670
Timo Bauer <timo.bauer97+github@gmail.com>
2022-01-23 19:43:30 +0100

    added getTemperatureLevelState

--- BoschSmartHomeSharp/ClimateControl.cs
+++ BoschSmartHomeSharp/ClimateControl.cs
@@ -183,13 +183,74 @@ namespace BoschSmartHome.ClimateControl
             {
                 //TODO Exception Handling
                 RoomClimateControlState roomClimateControlState = RoomClimateControlState.Deserialize(response.Content);
-                Debug.WriteLine($"found TemperatureLevel {deviceId}.");
+                Debug.WriteLine($"found RoomClimateControlState {deviceId}.");
 
                 return roomClimateControlState;
             }
             else
             {
-                Debug.WriteLine($"Could not fetch TemperatureLevel. Statuscode: {response.StatusCode} ({response.StatusDescription}) content: {response.Content}. Exception: {response.ErrorException}");
+                Debug.WriteLine($"Could not fetch RoomClimateControlState. Statuscode: {response.StatusCode} ({response.StatusDescription}) content: {response.Content}. Exception: {response.ErrorException}");
+
+                return null;
+            }
+        }
+
+        /// <summary>
+        ///     retrives the temperature level state from the Smarthome Controller. <br />
+        ///     the Certificate must be paired with the Controller. Otherwise the operation will fail (see <see cref="registerDevice(string, string, string, string)"/>)
+        /// </summary>
+        /// <param name="credentials">the credentials for the communication with the Smarthome Controller</param>
+        /// <param name="deviceId">the device</param>
+        /// <returns>
+        ///     <see cref="TemperatureLevelState"/><br />
+        ///     <b>value</b>: the received data
+        ///     <b>null</b>: the request failed. See Debug-log for more informations.
+        /// </returns>
+        public static TemperatureLevelState getTemperatureLevelState(ApiClient credentials, Device device)
+        {
+            return getTemperatureLevelState(credentials, device?.id);
+        }
+
+        /// <summary>
+        ///     retrives the temperature level state from the Smarthome Controller. <br />
+        ///     the Certificate must be paired with the Controller. Otherwise the operation will fail (see <see cref="registerDevice(string, string, string, string)"/>)
+        /// </summary>
+        /// <param name="credentials">the credentials for the communication with the Smarthome Controller</param>
+        /// <param name="deviceId">the id of the device</param>
+        /// <returns>
+        ///     <see cref="TemperatureLevelState"/><br />
+        ///     <b>value</b>: the received data
+        ///     <b>null</b>: the request failed. See Debug-log for more informations.
+        /// </returns>
+        public static TemperatureLevelState getTemperatureLevelState(ApiClient credentials, string deviceId)
+        {
+            // TODO Refactor.. NOTE: Different Ports for /smarthome/clients, /smarthome/ /remote/json-rpc, /public/ ...
+            RestClient client = new RestClient("https://" + credentials.IPaddress + $":8444/smarthome/devices/{deviceId}/services/TemperatureLevel/state")
+            {
+                RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true,
+                ClientCertificates = new X509CertificateCollection() { credentials.Certificate },
+                Timeout = -1
+            };
+
+            var request = new RestRequest(Method.GET);
+
+            //Request Header
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("api-version", "2.1");
+
+            IRestResponse response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                //TODO Exception Handling
+                TemperatureLevelState temperatureLevelState = TemperatureLevelState.Deserialize(response.Content);
+                Debug.WriteLine($"found TemperatureLevelState {deviceId}.");
+
+                return temperatureLevelState;
+            }
+            else
+            {
+                Debug.WriteLine($"Could not fetch TemperatureLevelState. Statuscode: {response.StatusCode} ({response.StatusDescription}) content: {response.Content}. Exception: {response.ErrorException}");
 
                 return null;
             }
