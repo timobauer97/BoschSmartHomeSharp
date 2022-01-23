35648de6ddd693fa0a8fa0995061cde0803a33f6
Timo Bauer <timo.bauer97+github@gmail.com>
2022-01-23 19:49:22 +0100

    added setRoomClimateControlState

--- BoschSmartHomeSharp/ClimateControl.cs
+++ BoschSmartHomeSharp/ClimateControl.cs
@@ -17,7 +17,7 @@ namespace BoschSmartHome.ClimateControl
         ///     the Certificate must be paired with the Controller. Otherwise the operation will fail (see <see cref="registerDevice(string, string, string, string)"/>)
         /// </summary>
         /// <param name="credentials">the credentials for the communication with the Smarthome Controller</param>
-        /// <param name="deviceId">the device</param>
+        /// <param name="device">the device</param>
         /// <returns>
         ///     <see cref="RoomClimateControl"/><br />
         ///     <b>value</b>: the received data
@@ -78,7 +78,7 @@ namespace BoschSmartHome.ClimateControl
         ///     the Certificate must be paired with the Controller. Otherwise the operation will fail (see <see cref="registerDevice(string, string, string, string)"/>)
         /// </summary>
         /// <param name="credentials">the credentials for the communication with the Smarthome Controller</param>
-        /// <param name="deviceId">the device</param>
+        /// <param name="device">the device</param>
         /// <returns>
         ///     <see cref="TemperatureLevel"/><br />
         ///     <b>value</b>: the received data
@@ -139,7 +139,7 @@ namespace BoschSmartHome.ClimateControl
         ///     the Certificate must be paired with the Controller. Otherwise the operation will fail (see <see cref="registerDevice(string, string, string, string)"/>)
         /// </summary>
         /// <param name="credentials">the credentials for the communication with the Smarthome Controller</param>
-        /// <param name="deviceId">the device</param>
+        /// <param name="device">the device</param>
         /// <returns>
         ///     <see cref="RoomClimateControlState"/><br />
         ///     <b>value</b>: the received data
@@ -200,7 +200,7 @@ namespace BoschSmartHome.ClimateControl
         ///     the Certificate must be paired with the Controller. Otherwise the operation will fail (see <see cref="registerDevice(string, string, string, string)"/>)
         /// </summary>
         /// <param name="credentials">the credentials for the communication with the Smarthome Controller</param>
-        /// <param name="deviceId">the device</param>
+        /// <param name="device">the device</param>
         /// <returns>
         ///     <see cref="TemperatureLevelState"/><br />
         ///     <b>value</b>: the received data
@@ -253,6 +253,79 @@ namespace BoschSmartHome.ClimateControl
                 Debug.WriteLine($"Could not fetch TemperatureLevelState. Statuscode: {response.StatusCode} ({response.StatusDescription}) content: {response.Content}. Exception: {response.ErrorException}");
 
                 return null;
+            }
+        }
+
+        /// <summary>
+        ///     Sets the temperature state of a Bosch Smarthome thermostate. <br />
+        ///     the Certificate must be paired with the Controller. Otherwise the operation will fail (see <see cref="registerDevice(string, string, string, string)"/>)
+        /// </summary>
+        /// <param name="credentials">the credentials for the communication with the Smarthome Controller</param>
+        /// <param name="device">the device.</param>
+        /// <param name="setpointTemperature">
+        ///     <see cref="double"/> <br />
+        ///     <b>value</b>: the setpoint temperature
+        /// </param>
+        /// <returns>
+        ///     <see cref="bool"/> <br />
+        ///     <b>true</b>: the state was successfully set <br />
+        ///     <b>false</b>: The request failed. See Debug-log for more informations.
+        /// </returns>
+        public static bool setRoomClimateControlState(ApiClient credentials, Device device, double setpointTemperature)
+        {
+            return setRoomClimateControlState(credentials, device?.id, setpointTemperature);
+        }
+
+        /// <summary>
+        ///     Sets the temperature state of a Bosch Smarthome thermostate. <br />
+        ///     the Certificate must be paired with the Controller. Otherwise the operation will fail (see <see cref="registerDevice(string, string, string, string)"/>)
+        /// </summary>
+        /// <param name="credentials">the credentials for the communication with the Smarthome Controller</param>
+        /// <param name="deviceId">the id of the device.</param>
+        /// <param name="setpointTemperature">
+        ///     <see cref="double"/> <br />
+        ///     <b>value</b>: the setpoint temperature
+        /// </param>
+        /// <returns>
+        ///     <see cref="bool"/> <br />
+        ///     <b>true</b>: the state was successfully set <br />
+        ///     <b>false</b>: The request failed. See Debug-log for more informations.
+        /// </returns>
+        public static bool setRoomClimateControlState(ApiClient credentials, string deviceId, double setpointTemperature)
+        {
+            // TODO Refactor.. NOTE: Different Ports for /smarthome/clients, /smarthome/ /remote/json-rpc, /public/ ...
+            RestClient client = new RestClient("https://" + credentials.IPaddress + $":8444/smarthome/devices/{deviceId}/services/RoomClimateControl/state")
+            {
+                RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true,
+                ClientCertificates = new X509CertificateCollection() { credentials.Certificate },
+                Timeout = -1
+            };
+
+            var request = new RestRequest(Method.PUT);
+
+            //Request Header
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("api-version", "2.1");
+
+            //Request Body
+            request.AddParameter("application/json",
+                RoomClimateControlState.Serialize(
+                    new RoomClimateControlState { Type = "climateControlState", setpointTemperature = setpointTemperature.ToString() }),
+                ParameterType.RequestBody);
+
+            IRestResponse response = client.Execute(request);
+
+            // No-Content is OK
+            if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+            {
+                Debug.WriteLine($"set temperature state of device {deviceId} to {setpointTemperature}");
+                return true;
+            }
+            else
+            {
+                Debug.WriteLine($"Could not set temperature state. Statuscode: {response.StatusCode} ({response.StatusDescription}) content: {response.Content}. Exception: {response.ErrorException}");
+
+                return false;
             }
         }
     }
