1c6ea315a0255049651c99b52b02b29d4e94004f
Timo Bauer <timo.bauer97+github@gmail.com>
2022-01-24 18:39:11 +0100

    added LongPolling.cs

---
+++ BoschSmartHomeSharp/mdl/LongPolling/LongPolling.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoschSmartHomeSharp.mdl.LongPolling
+{
+    public class LongPolling
+    {
+        public string jsonrpc { get; set; }
+        public string method { get; set; }
+        public List<string> @params { get; set; }
+
+        public static string Serialize(LongPolling longPolling)
+        {
+            return JsonConvert.SerializeObject(longPolling);
+        }
+
+        public static LongPolling Deserialize(string json)
+        {
+            return JsonConvert.DeserializeObject<LongPolling>(json);
+        }
+    }
+}
