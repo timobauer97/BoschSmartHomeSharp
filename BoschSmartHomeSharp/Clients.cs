0d72f962375846edc16722a5eab00da75462c946
Timo Bauer <timo.bauer97+github@gmail.com>
2022-01-24 17:28:16 +0100

    refactoring

---
+++ BoschSmartHomeSharp/Clients.cs
@@ -0,0 +1,67 @@
+﻿using BoschSmartHome.mdl.RegisterDevice;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using static BoschSmartHomeSharp;
+
+namespace BoschSmartHome.Clients
+{
+    public static class Clients
+    {
+        /// <summary>
+        ///     Registers a client in the Bosch Smarthome Controller. <br />
+        ///     <b>NOTE: Before this method is called, you need to press the pairing-button until the LEDs start flashing.</b>
+        /// </summary>
+        /// <param name="devicePwdBase64">the base64 encoded password of the controller.</param>
+        /// <param name="cert">
+        ///     the content of the certificate (begins with -----BEGIN CERTIFICATE-----).<br />
+        ///     <b>NOTE: As <a href="https://github.com/BoschSmartHome/bosch-shc-api-docs/tree/master/postman#customize-the-certificate">mentioned in the official GitHub repo</a>, the certificate might be modified!</b>
+        /// </param>
+        /// <param name="clientId">the id of the client</param>
+        /// <param name="clientName">the name of the client</param>
+        /// <returns>
+        ///     <see cref="bool"/><br /> 
+        ///     <b>true:</b> the client has been successfully registered.<br />
+        ///     <b>false:</b> an error occured. See Debug-log for more informations.
+        /// </returns>
+        public static bool registerClient(ApiClient credentials, string devicePwdBase64, string cert, string clientId, string clientName)
+        {
+            RestClient client = new RestClient("https://" + credentials.IPaddress + ":8443/smarthome/clients")
+            {
+                RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true,
+                ClientCertificates = new X509CertificateCollection() { credentials.Certificate },
+                Timeout = -1
+            };
+
+            RestRequest request = new RestRequest(Method.POST);
+
+            // Request Header
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Systempassword", devicePwdBase64);
+
+            // Request Body
+            request.AddParameter("application/json",
+                RegisterDevice.Serialize(
+                    new RegisterDevice { Type = "client", id = $"oss_{clientId}", name = $"OSS {clientName}", primaryRole = "ROLE_RESTRICTED_CLIENT", certificate = cert }),
+                ParameterType.RequestBody);
+
+            IRestResponse response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Created)
+            {
+                Debug.WriteLine("client registration was successfull");
+                return true;
+            }
+            else
+            {
+                Debug.WriteLine($"client registration failed with Statuscode {response.StatusCode} ({response.StatusDescription}) and content {response.Content}. Exception: {response.ErrorException}");
+                return false;
+            }
+
+
+        }
+    }
+}
---
+++ BoschSmartHomeSharp/Devices.cs
@@ -0,0 +1,56 @@
+﻿using BoschSmartHome.mdl.Device;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using static BoschSmartHomeSharp;
+
+namespace BoschSmartHome.Devices
+{
+    public static class Devices
+    {
+        /// <summary>
+        ///     Fetches the device list from the Bosch Smarthome Controller.<br />
+        ///     the Certificate must be paired with the Controller. Otherwise the operation will fail (see <see cref="registerClient(string, string, string, string)"/>)
+        /// </summary>
+        /// <returns>
+        ///     <b>List with <see cref="Device"/></b>: Contains all devices, paired with the Smarthome Controller.<br />
+        ///     <b>null</b>: The request failed. See Debug-log for more informations.
+        /// </returns>
+        public static List<Device> getDevices(ApiClient credentials)
+        {
+            // TODO Refactor.. NOTE: Different Ports for /smarthome/clients, /smarthome/ /remote/json-rpc, /public/ ...
+            RestClient client = new RestClient("https://" + credentials.IPaddress + ":8444/smarthome/devices")
+            {
+                RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true,
+                ClientCertificates = new X509CertificateCollection() { credentials.Certificate },
+                Timeout = -1
+            };
+
+            var request = new RestRequest(Method.GET);
+
+            //Request Header
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("api-version", "2.1");
+
+            IRestResponse response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                //TODO Exception Handling
+                List<Device> devices = Device.DeserializeList(response.Content);
+                Debug.WriteLine($"found {devices.Count} devices.");
+
+                return devices;
+            }
+            else
+            {
+                Debug.WriteLine($"Could not fetch devices. Statuscode: {response.StatusCode} ({response.StatusDescription}) content: {response.Content}. Exception: {response.ErrorException}");
+
+                return null;
+            }
+        }
+    }
+}
