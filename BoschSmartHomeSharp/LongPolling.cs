de6f8d9958ecb69e08859986b7c50d0f046d893d
Timo Bauer <timo.bauer97+github@gmail.com>
2022-01-24 17:21:49 +0100

    Added structure for LongPolling

---
+++ BoschSmartHomeSharp/LongPolling.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoschSmartHome.LongPolling
+{
+    public static class LongPolling
+    {
+
+        public static string subscribe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string poll(string pollId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string unsubscribe(string pollId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
